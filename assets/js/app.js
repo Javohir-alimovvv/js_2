4484d822456f534febe958d19569121b07909666
javohir <ajavohir969@gmail.com>
2024-02-03 22:59:20 +0500

    menu

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+
+
+
+
+const btnEl = document.querySelector(".btn");
+const menuEl = document.querySelector(".menu");
+btnEl.addEventListener("click", () => {
+    menuEl.style.display = "block";
+});
