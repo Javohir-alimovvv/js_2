3f072f1546b3322c87d7ee27751dcefd380cfa11
javohir <ajavohir969@gmail.com>
2024-02-04 18:07:26 +0500

    menu

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,15 @@
 
 
 const btnEl = document.querySelector(".btn");
+const btn_2El = document.querySelector(".btn_2");
 const menuEl = document.querySelector(".menu");
 btnEl.addEventListener("click", () => {
     menuEl.style.display = "block";
+    btnEl.style.display = "none";
+    btn_2El.style.display = "block";
+});
+btn_2El.addEventListener("click", () => {
+    menuEl.style.display = "none";
+    btnEl.style.display = "block";
+    btn_2El.style.display = "none";
 });
